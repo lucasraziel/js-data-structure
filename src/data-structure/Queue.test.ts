b746ef88efb9e9d6151108c513c87830c32fe344
Lucas Rego <lucas.raziel@gmail.com>
2022-03-03 13:36:14 -0300

    Created the Queue Data structure
    Used linked list for implementation of the queue data structure
    
    addded plugin for prettier eslint

--- src/data-structure/Queue.test.ts
+++ src/data-structure/Queue.test.ts
@@ -1,9 +1,107 @@
 import Queue from './Queue';
 
-describe('Queue', ()=>{
-    it('should create an object of type Queue', ()=>{
-        const queue = new Queue();
+describe('Queue', () => {
+  it('should create an object of type Queue', () => {
+    const queue = new Queue();
 
-        expect(queue).toBeInstanceOf(Queue);
-    })
-})
+    expect(queue).toBeInstanceOf(Queue);
+  });
+
+  it('should enqueue an item', () => {
+    const queue = new Queue();
+
+    queue.enqueue(1);
+
+    expect(queue.length()).toBe(1);
+  });
+
+  it('should enqueue two items', () => {
+    const queue = new Queue();
+
+    queue.enqueue(1);
+
+    queue.enqueue(2);
+
+    expect(queue.length()).toBe(2);
+  });
+
+  it('should dequeue a two items queue', () => {
+    const queue = new Queue();
+
+    queue.enqueue(1);
+
+    queue.enqueue(2);
+
+    expect(queue.dequeue()).toBe(1);
+
+    expect(queue.length()).toBe(1);
+  });
+
+  it('should dequeue a one item queue', () => {
+    const queue = new Queue();
+
+    queue.enqueue(1);
+
+    expect(queue.dequeue()).toBe(1);
+
+    expect(queue.length()).toBe(0);
+  });
+
+  it('should dequeue a no item queue', () => {
+    const queue = new Queue();
+
+    expect(queue.dequeue()).toBe(null);
+
+    expect(queue.length()).toBe(0);
+  });
+
+  it('should get the correct peek of no item queue', () => {
+    const queue = new Queue();
+
+    expect(queue.peek()).toBe(null);
+  });
+
+  it('should get the correct peek of a queue', () => {
+    const queue = new Queue();
+
+    queue.enqueue(1);
+
+    queue.enqueue(2);
+
+    expect(queue.peek()).toBe(1);
+  });
+
+  it('should print the queue', () => {
+    const printQueue = jest.fn();
+    const queue = new Queue();
+
+    queue.enqueue(1);
+
+    queue.enqueue(2);
+
+    queue.print(printQueue);
+
+    expect(printQueue).toHaveBeenCalledTimes(2);
+
+    expect(printQueue).toHaveBeenCalledWith(1);
+
+    expect(printQueue).toHaveBeenCalledWith(2);
+  });
+
+  it('should print the queue with console.log', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    const queue = new Queue();
+
+    queue.enqueue(1);
+
+    queue.enqueue(2);
+
+    queue.print();
+
+    expect(consoleSpy).toHaveBeenCalledTimes(2);
+
+    expect(consoleSpy).toHaveBeenCalledWith(1);
+
+    expect(consoleSpy).toHaveBeenCalledWith(2);
+  });
+});
--- src/data-structure/Queue.ts
+++ src/data-structure/Queue.ts
@@ -1,3 +1,85 @@
-export default class Queue {
-    
+interface NodeItem<T> {
+  value: T;
+  next: NodeItem<T> | null;
+}
+interface SetQueue<T> {
+  [key: symbol]: NodeItem<T> | null;
+}
+
+export default class Queue<T = Number> {
+  start: symbol;
+
+  end: symbol;
+
+  set: SetQueue<T>;
+
+  constructor() {
+    this.start = Symbol('start');
+    this.end = Symbol('end');
+    this.set = {};
+    this.set[this.start] = null;
+    this.set[this.end] = null;
+  }
+
+  isEmpty(): boolean {
+    return this.set[this.start] === null;
+  }
+
+  enqueue(item: T): void {
+    if (this.isEmpty()) {
+      this.set[this.start] = {
+        value: item,
+        next: null,
+      };
+      this.set[this.end] = this.set[this.start];
+    } else {
+      this.set[this.end].next = {
+        value: item,
+        next: null,
+      };
+      this.set[this.end] = this.set[this.end].next;
+    }
+  }
+
+  length(): number {
+    let current = this.set[this.start];
+    let count = 0;
+    while (current !== null) {
+      current = current.next;
+      count++;
+    }
+    return count;
+  }
+
+  dequeue(): T | null {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const item = this.set[this.start].value;
+    this.set[this.start] = this.set[this.start].next;
+    if (this.set[this.start] === null) {
+      this.set[this.end] = null;
+    }
+    return item;
+  }
+
+  peek(): T | null {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.set[this.start].value;
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  print(printFn?: (item: T) => void): void {
+    let current = this.set[this.start];
+    while (current !== null) {
+      if (printFn) {
+        printFn(current.value);
+      } else {
+        console.log(current.value);
+      }
+      current = current.next;
+    }
+  }
 }
